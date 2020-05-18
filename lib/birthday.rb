a4cfd2536f43dbedecedeb6d2df7da8b35cf9a14
Sha-jay Perry <shajayjanae@gmail.com>
2020-05-18 00:17:02 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -12,4 +12,11 @@ end
 
 def 
 
-
+def age_appropriate_birthday(birthday_kids)
+   birthday_kids.each do |kids_name, age|
+     if age <= 12
+     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+     else
+       puts "You are to old for this."
+  end
+end
